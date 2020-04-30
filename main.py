53acbab371b538bbcf84eeeb744c4122c42f27b2
alchrabas <alchrabas@exeris.org>
2020-04-30 13:12:39 +0200

    Calculate elapsed time

--- main.py
+++ main.py
@@ -7,39 +7,50 @@ import plot
 from world_generation import voronoi, mountain_chains, heightmap
 import world_generation
 
+
+def time_from_last_checkpoint() -> float:
+    """
+    Generator returning seconds elapsed since the previous call of the generator
+    """
+    start = time.time()
+    yield 0.0
+    while True:
+        last_checkpoint = time.time()
+        yield last_checkpoint - start
+        start = last_checkpoint
+
+
 start = time.time()
 
 number_of_points = 2000
 bounding_box = [(0, 1), (0, 1)]
 
-voronoi_diag = voronoi.relaxed_voronoi(number_of_points, bounding_box, 5)
-after_v = time.time()
-world = data.convert_to_world(voronoi_diag.filtered_regions, voronoi_diag.filtered_points, voronoi_diag.vertices)
-after_conv = time.time()
-world_generation.mountain_chains.create_mountain_chains(7, world)
+checkpoint = time_from_last_checkpoint()
+next(checkpoint)
 
+voronoi_diag = voronoi.relaxed_voronoi(number_of_points, bounding_box, 5)
+print("voronoi", next(checkpoint))
+
+world = data.convert_to_world(voronoi_diag.filtered_regions, voronoi_diag.filtered_points, voronoi_diag.vertices)
+print("conversion", next(checkpoint))
+
+world_generation.mountain_chains.create_mountain_chains(7, world)
 world.height_by_region = world_generation.heightmap.create_heightmap(world)
-after_height = time.time()
+print("heightmap", next(checkpoint))
 
 world.terrain_blobs = data.merge_heights_into_blobs(world)
-after_blob = time.time()
+print("convert to blobs", next(checkpoint))
+
 data.fix_mountain_center_line_to_fully_cover_mountain_polygon(world)
-after_fix = time.time()
+print("fix mountains", next(checkpoint))
+
 dump = exporter.export(world.terrain_blobs)
 
 with open("/tmp/terrains.json", "w") as file:
     file.write(dump)
+print("export", next(checkpoint))
 
-after_export = time.time()
 plot.plot_hypsometric(world)
-after_plot = time.time()
+print("plot", next(checkpoint))
 
-print("voronoi", after_v - start)
-print("conv", after_conv - after_v)
-print("hei", after_height - after_conv)
-print("blob", after_blob - after_height)
-print("fix mountains", after_fix - after_blob)
-print("export", after_export - after_fix)
-print("plot", after_plot - after_export)
-
-print("Finished in ", time.time() - start, "seconds")
+print("Finished in {} seconds".format(time.time() - start))
